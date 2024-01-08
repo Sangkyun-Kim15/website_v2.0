f8e035d0a07579ae3bfbf0b45de475da41e8ee12
sangkyun <sangkyun@LAPTOP-U264MDMD>
2024-01-08 13:32:19 -0800

    delete unused import in videoController.java

--- src/main/java/kr/co/mlec/Video/VideoController.java
+++ src/main/java/kr/co/mlec/Video/VideoController.java
@@ -3,8 +3,6 @@ package kr.co.mlec.Video;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
